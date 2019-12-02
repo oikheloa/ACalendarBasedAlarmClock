aac083a41309c2a2088bb785a260fb38d20c4861
catherinelau <catherinelau.home@gmail.com>
2019-12-02 16:29:38 -0500

    Refactoring Transportation

--- app/src/main/java/com/example/alarmclock/Transportation.java
+++ app/src/main/java/com/example/alarmclock/Transportation.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TransportationForm extends AppCompatActivity {
+public class Transportation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transportation_form);
+        setContentView(R.layout.activity_transportation);
     }
 }
